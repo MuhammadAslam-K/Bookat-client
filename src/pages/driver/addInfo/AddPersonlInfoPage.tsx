35c3b99b83ec7c8dd250e50ca178246f89d60427
MuhammadAslam-K <aslamka.2k3@gmail.com>
2023-11-12 23:01:45 +0530

    Add Firebase workflow files

--- src/pages/driver/addInfo/AddPersonlInfoPage.tsx
+++ src/pages/driver/addInfo/AddPersonlInfoPage.tsx
@@ -1,10 +1,14 @@
+import Navbar from "../../../components/driver/common/Navbar"
+import DriverFooter from "../../../components/driver/common/DriverFooter"
 import AddPersonalInfo from "../../../components/driver/AddPersonalAndVehicleInfo/AddPersonalInfo"
 
 function AddPersonlInfoPage() {
     return (
-        <>
+        <div className="bg-gray-100">
+            <Navbar />
             <AddPersonalInfo />
-        </>
+            <DriverFooter />
+        </div>
     )
 }
 
